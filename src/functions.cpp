4f7c6e23bd0b1668fcec830daad35c51d1b472c4
Andrey <grizzly-terror@yandex.ru>
2014-06-03 19:39:46 +0400

    change geometry, add static functions for moving

---
+++ src/functions.cpp
@@ -0,0 +1,37 @@
+#include "functions.h"
+#include <cmath>
+double functions::sin(double x){
+    return sin(x);
+}
+
+double functions::cos(double x){
+    return cos(x);
+}
+
+double functions::ln(double x){
+    return ln(x);
+}
+
+double functions::pow2(double x){
+    return x*x;
+}
+
+double functions::x2(double x){
+    return 2*x;
+}
+
+double functions::x10(double x){
+    return 10*x;
+}
+
+double functions::plus16(double x){
+    return x+16;
+}
+
+double functions::minus(double x){
+    return -x;
+}
+
+double functions::x(double x){
+    return x;
+}
---
+++ src/functions.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+struct functions{
+    static double sin(double);
+    static double cos(double);
+    static double ln(double);
+    static double pow2(double);
+    static double x2(double);
+    static double x10(double);
+    static double plus16(double);
+    static double minus(double);
+    static double x(double);
+};
+
+#endif // FUNCTIONS_H
